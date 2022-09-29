0575dae6d457a7eca341a0b9bccdbbd9656eb008
trondrift <nhansen.dev@gmail.com>
2022-09-29 19:48:58 +0000

    Deploying to gh-pages from  @ 7994e9379c9fbc347158a7e2b08a20826a14f0d6 🚀

---
+++ precache-manifest.1def432ad1860ab80a874883b258b250.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d529d1c2e01433794457444b432f5d59",
+    "url": "/N8bit-profile/index.html"
+  },
+  {
+    "revision": "3df4af718b181987cd40",
+    "url": "/N8bit-profile/static/css/3.4d5083f8.chunk.css"
+  },
+  {
+    "revision": "60a5ef27d5488fb7349c",
+    "url": "/N8bit-profile/static/css/4.fbe33953.chunk.css"
+  },
+  {
+    "revision": "38f59e68a66bbec6ca4a",
+    "url": "/N8bit-profile/static/css/main.d89a87aa.chunk.css"
+  },
+  {
+    "revision": "b0ef3fcc585e28f53863",
+    "url": "/N8bit-profile/static/js/2.8c953dba.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/N8bit-profile/static/js/2.8c953dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df4af718b181987cd40",
+    "url": "/N8bit-profile/static/js/3.62f55009.chunk.js"
+  },
+  {
+    "revision": "60a5ef27d5488fb7349c",
+    "url": "/N8bit-profile/static/js/4.6394435e.chunk.js"
+  },
+  {
+    "revision": "38f59e68a66bbec6ca4a",
+    "url": "/N8bit-profile/static/js/main.e070772f.chunk.js"
+  },
+  {
+    "revision": "09d3c52ce32ed392f83a",
+    "url": "/N8bit-profile/static/js/runtime-main.48dec439.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/N8bit-profile/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "5dba203162d0f27f79cde0019dff0c54",
+    "url": "/N8bit-profile/static/media/DCDetourProfile.5dba2031.png"
+  },
+  {
+    "revision": "cde4fb567872712b960a7ca715133767",
+    "url": "/N8bit-profile/static/media/HULogo.cde4fb56.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/N8bit-profile/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "7f47daaed88278e3975439eedca15355",
+    "url": "/N8bit-profile/static/media/SSLogo.7f47daae.png"
+  },
+  {
+    "revision": "e8d2eb009e0eeec8882d66cb9b7a6c84",
+    "url": "/N8bit-profile/static/media/WHcombo.e8d2eb00.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/N8bit-profile/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/N8bit-profile/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "4b9366850c5139088eff431bbb72c1ed",
+    "url": "/N8bit-profile/static/media/cwlogo.4b936685.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/N8bit-profile/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "716868de399818ed4ae20619e4bbb8a6",
+    "url": "/N8bit-profile/static/media/gentexblack.716868de.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/N8bit-profile/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "cf087d6ed14c4cbdf176312443dcd3c3",
+    "url": "/N8bit-profile/static/media/gv2full.cf087d6e.png"
+  },
+  {
+    "revision": "57b6a725c7561f19b18704d87a6ca600",
+    "url": "/N8bit-profile/static/media/hansenchartssq.57b6a725.png"
+  },
+  {
+    "revision": "e1e2107faebd43efd4413e17b8d9a44c",
+    "url": "/N8bit-profile/static/media/hanstechLLC2.e1e2107f.png"
+  },
+  {
+    "revision": "42e5ed216d18586240dec4314ce9a040",
+    "url": "/N8bit-profile/static/media/interlochen.42e5ed21.png"
+  },
+  {
+    "revision": "a36224e101f2ba915cef629c31cb0afc",
+    "url": "/N8bit-profile/static/media/melo.a36224e1.png"
+  },
+  {
+    "revision": "1268cf878f3cba995e54b2e5b6e028dd",
+    "url": "/N8bit-profile/static/media/profile-pic5.1268cf87.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/N8bit-profile/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/N8bit-profile/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "e95d1056842068d9047b1bea230e2765",
+    "url": "/N8bit-profile/static/media/talksCardBack.e95d1056.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.50d24fdd0c6fa4e263499ac2b28dfee3.js"
+  "/N8bit-profile/precache-manifest.1def432ad1860ab80a874883b258b250.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/N8bit-profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
